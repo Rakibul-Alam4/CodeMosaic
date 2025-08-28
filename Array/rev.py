8d82df5572cc070a85afe47f194f77e56fac4d7d
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-08-28 11:23:13 +0600

    Updated

---
+++ Array/rev.py
@@ -0,0 +1,17 @@
+"""
+#
+shorting the number
+num= [2,3,4,5,1,11]
+num.sort()
+print(num)
+"""
+
+"""num= [2,3,4,5,1,11]
+num.sort(reverse = True)
+print(num)
+"""
+
+num1=['a','b','d']
+num2=[1,2,3]
+num1.extend(num2)
+print(num1) 
