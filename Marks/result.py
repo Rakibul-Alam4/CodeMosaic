39bef32168ec15ffe1568dfafddffa6c6564d741
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-09-18 10:26:46 +0600

    marks

---
+++ Marks/result.py
@@ -0,0 +1,12 @@
+print("Student Info")
+marks = int (input("Enter the marks: "))
+if marks>=80:
+ print("A+")
+elif marks >= 70:
+ print("A")
+elif marks >= 60:
+ print("B")
+elif marks >= 50:
+ print("C")
+else:
+ print("Fail")
