95931e8086b1d04e4ad0a31d700571b5960f690a
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-09-20 10:46:02 +0600

    Add files via upload

---
+++ Max_Min/mx_mn.py
@@ -0,0 +1,11 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Compare the numbers and print which one is bigger
+if num1 > num2:
+    print("The first number ({num1}) is bigger.")
+elif num2 > num1:
+    print("The second number ({num2}) is bigger.")
+else:
+    print("Both numbers are equal.")
+    
