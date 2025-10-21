9908272c6f433367b45612d54c3c6016dc3bafc8
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-10-21 11:36:57 +0600

    Add files via upload

---
+++ Number/oddeven.py
@@ -0,0 +1,9 @@
+number=int(input("Enter the numbber:"))
+if number > 0:
+    print("Positive Number")
+elif number<0:
+    print("Negative Number")
+elif number==0:
+    print("Zero")
+else:
+    print("Invalid")
