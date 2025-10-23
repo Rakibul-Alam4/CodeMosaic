f8d6a34dcae4de0e39e29c0bcd42b146cfc96252
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-10-23 11:35:53 +0600

    Add files via upload

---
+++ Prblm/add.py
@@ -0,0 +1,17 @@
+'''
+# Area of triangle:
+
+base = int(input("enter the base: "))
+height = int(input("enter the height: "))
+area = 0.5*base*height
+print("Area of triangle is:", area)
+'''
+"""# Area of circle
+radius=int (input("Enter the radius:"))
+circle= 3.1416*radius*radius
+print("The Circle is:", circle)
+"""
+#Temparature
+Fer=int (input("Enter the fer:"))
+Cilcius= (Fer-32)/1.8
+print("The Cilcius is:",Cilcius)
