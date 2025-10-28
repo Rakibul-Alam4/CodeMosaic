de0446f3c23f903a3b2fc6bc27fec27e69a3f54f
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-10-28 12:13:38 +0600

    Add files via upload

---
+++ test/3sum.py
@@ -0,0 +1,14 @@
+"""
+num1 = int(input("enter the num1: "))
+num2 = int(input("enter the num2: "))
+num3 = int(input("enter the num3: "))
+
+if num1>num2 and num2>num3:
+    print("Num1 is the bigger", num1)
+elif num2>num3 and num2>num1:
+    print("Num2 is the bigger", num2)
+elif num3>num2 and num3>num1:
+    print("Num3 is the bigger", num3)
+"""
+
+
