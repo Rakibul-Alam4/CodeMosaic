b22a395c25336adc6a1c2bdfd394758f858a8608
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-10-29 11:07:18 +0600

    Add files via upload

---
+++ test/detnry.py
@@ -0,0 +1,13 @@
+thisdit = {
+    "samir":{
+        "name": "karim",
+        "phn": 177,
+        "Vill": "Cumilla"
+    },
+    "karim":{
+        "name": "karim",
+        "Add": "Dhaka"
+    }
+}
+
+print(thisdit)
