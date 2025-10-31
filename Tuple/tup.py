ff4d571ab184a8ac416c1714d1c0319d449cfa7e
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-10-31 14:21:14 +0600

    python tuple

---
+++ Tuple/tup.py
@@ -0,0 +1,31 @@
+"""
+# Tupple immutable, unchangeable
+#Tupple print value in negative (-)sign
+tup=(1,2,3,4,"cs","cv")
+print(tup[-1])
+"""
+
+"""
+tupp = (1,2,3,4,5,6,7,8,)
+print(tupp[1:5])
+"""
+
+"""thisTuple=("ahad", "sakil", "johan")
+a=list(thisTuple)
+a.append("Rony")
+thisTuple=tuple(a)
+print(thisTuple)
+"""
+
+"""#Unpack_Tuples
+fruits=("apple","banana","mango","chery","orange")
+(a,b,*c) = fruits
+print(c)
+"""
+
+#loop in tuples
+fruits=("apple","banana","mango","chery","orange")
+for i in fruits:
+    print(i)
+for x in range(len(fruits)):
+    print(x)
